190e762df621564e37a91d79f2fcd7dd24df8ca0
Kenneth van der Werf <kennethwerf@gmail.com>
2020-07-29 15:26:11 +0200

    Initial GraphQL example

---
+++ src/test/java/com/codinginspace/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codinginspace.books;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
